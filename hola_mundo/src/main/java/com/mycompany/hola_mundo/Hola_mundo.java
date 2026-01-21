d54e79b3163f3803e2695e46580675295343c349
Legna25 <angelgithub2545@gmail.com>
2026-01-21 13:35:47 +0100

    Add files via upload

---
+++ hola_mundo/src/main/java/com/mycompany/hola_mundo/Hola_mundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola_mundo;
+
+/**
+ *
+ * @author angel
+ */
+public class Hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
